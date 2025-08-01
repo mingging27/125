524df10db635231d11febed2d4679c6d5a512ccc
getpoorpoo <getpoorpoo@gmail.com>
2025-08-01 20:08:58 +0900

    fix: 모델 정의 SQL 제약조건과 일치하도록 수정

--- server/models/jobpost.js
+++ server/models/jobpost.js
@@ -74,7 +74,7 @@
 //         onDelete: 'CASCADE',
 //       });
 //     }
-    
+
 //     if (db.Scrap) {
 //       db.JobPost.hasMany(db.Scrap, {
 //         foreignKey: 'job_post_id',
@@ -88,109 +88,111 @@
 //         foreignKey: 'job_post_id',
 //         sourceKey: 'job_post_id',
 //         onDelete: 'CASCADE',
-//       });      
+//       });
 //     }
 //   }
 // }
 
 // module.exports = JobPost;
 
-
 // models/jobPost.js
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class JobPost extends Model {
     static associate(db) {
       if (db.Application) {
         db.JobPost.hasMany(db.Application, {
-          foreignKey: 'job_post_id',
-          sourceKey: 'job_post_id',
-          onDelete: 'CASCADE',
+          foreignKey: "job_post_id",
+          sourceKey: "job_post_id",
+          onDelete: "CASCADE",
         });
       }
 
       if (db.Scrap) {
         db.JobPost.hasMany(db.Scrap, {
-          foreignKey: 'job_post_id',
-          sourceKey: 'job_post_id',
-          onDelete: 'CASCADE',
+          foreignKey: "job_post_id",
+          sourceKey: "job_post_id",
+          onDelete: "CASCADE",
         });
       }
 
       if (db.Comment) {
         db.JobPost.hasMany(db.Comment, {
-          foreignKey: 'job_post_id',
-          sourceKey: 'job_post_id',
-          onDelete: 'CASCADE',
+          foreignKey: "job_post_id",
+          sourceKey: "job_post_id",
+          onDelete: "CASCADE",
         });
       }
     }
   }
 
-  JobPost.init({
-    job_post_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
+  JobPost.init(
+    {
+      job_post_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      company_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      job_field: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      location_city: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      employment_type: {
+        type: DataTypes.ENUM("fullTime", "partTime", "contract"),
+        defaultValue: "fullTime",
+      },
+      work_hour: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      salary_info: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("read", "unread"),
+        defaultValue: "unread",
+      },
+      required_keyword: {
+        type: DataTypes.TEXT,
+      },
+      min_age: {
+        type: DataTypes.INTEGER,
+      },
+      max_age: {
+        type: DataTypes.INTEGER,
+      },
+      ai_match_result: {
+        type: DataTypes.TEXT,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      deadline: {
+        type: DataTypes.DATE,
+      },
     },
-    company_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    job_field: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    location_city: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    employment_type: {
-      type: DataTypes.ENUM('fullTime', 'partTime', 'contract'),
-      defaultValue: 'fullTime',
-    },
-    work_hour: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    salary_info: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM('read', 'unread'),
-      defaultValue: 'unread',
-    },
-    required_keword: {
-      type: DataTypes.TEXT,
-    },
-    min_age: {
-      type: DataTypes.INTEGER,
-    },
-    max_age: {
-      type: DataTypes.INTEGER,
-    },
-    ai_match_result: {
-      type: DataTypes.TEXT,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-    },
-    deadline: {
-      type: DataTypes.DATE,
-    },
-  }, {
-    sequelize,
-    modelName: 'JobPost',
-    tableName: 'JobPost',
-    timestamps: false,
-  });
+    {
+      sequelize,
+      modelName: "JobPost",
+      tableName: "JobPost",
+      timestamps: false,
+    }
+  );
 
   return JobPost;
 };
--- server/models/scrap.js
+++ server/models/scrap.js
@@ -1,60 +1,69 @@
 module.exports = (sequelize, DataTypes) => {
-  const Scrap = sequelize.define("Scrap", {
-    scrap_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+  const Scrap = sequelize.define(
+    "Scrap",
+    {
+      scrap_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      scrapped_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      post_type: {
+        type: DataTypes.ENUM("job", "info", "community"),
+        defaultValue: "job",
+      },
+      info_post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      job_post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      community_post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    scrapped_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    },
-    post_type: {
-      type: DataTypes.ENUM("job", "info", "community"),
-      defaultValue: "job"
-    },
-    info_post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    job_post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    community_post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {
-    tableName: "scrap",
-    timestamps: false
-  });
+    {
+      tableName: "scrap",
+      timestamps: false,
 
-  Scrap.associate = function(models) {
-  // 사용자와의 관계
+      indexes: [
+        {
+          unique: true,
+          fields: ["user_id", "community_post_id", "post_type"],
+        },
+      ],
+    }
+  );
+
+  Scrap.associate = function (models) {
+    // 사용자와의 관계
     Scrap.belongsTo(models.User, {
-      foreignKey: 'user_id',
-      as: 'user'
+      foreignKey: "user_id",
+      as: "user",
     });
 
     // 커뮤니티 게시글과의 관계
     Scrap.belongsTo(models.CommunityPost, {
-      foreignKey: 'community_post_id',
-      as: 'communityPost'
+      foreignKey: "community_post_id",
+      as: "communityPost",
     });
 
     // 정보글(infoPost)과의 관계
     Scrap.belongsTo(models.InfoPost, {
-      foreignKey: 'info_post_id',
-      as: 'infoPost'
+      foreignKey: "info_post_id",
+      as: "infoPost",
     });
   };
 
   return Scrap;
 };
-
-
