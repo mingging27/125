14879b0aed99835eff69387a265760cc1746f7ef
getpoorpoo <getpoorpoo@gmail.com>
2025-08-11 22:04:16 +0900

    refactor: Like 모델 db에 맞게 수정

--- server/models/like.js
+++ server/models/like.js
@@ -1,26 +1,43 @@
 module.exports = (sequelize, DataTypes) => {
-  const Like = sequelize.define('Like', {
-    like_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const Like = sequelize.define(
+    "Like",
+    {
+      like_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      liked_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      post_type: {
+        type: DataTypes.ENUM("job", "info", "community"),
+        allowNull: false,
+        defaultValue: "job",
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      community_post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      info_post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    community_post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  }, {
-    tableName: 'like',
-    timestamps: false,
-  });
+    {
+      tableName: "like",
+      timestamps: false,
+    }
+  );
 
   Like.associate = (models) => {
-    Like.belongsTo(models.User, { foreignKey: 'user_id' });
-    Like.belongsTo(models.CommunityPost, { foreignKey: 'community_post_id' });
+    Like.belongsTo(models.User, { foreignKey: "user_id" });
+    Like.belongsTo(models.CommunityPost, { foreignKey: "community_post_id" });
   };
 
   return Like;
